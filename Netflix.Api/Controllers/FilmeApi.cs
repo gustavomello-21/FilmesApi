00bd0f7ec44cb005bc63ee3a9ff78b7a3b05034d
Luis Gustavo Silva Melo <gustavo.melo@enjoei.com.br>
2022-11-20 16:48:50 -0300

    add controller

--- Netflix.Api/Controllers/FilmeApi.cs
+++ Netflix.Api/Controllers/FilmeApi.cs
@@ -14,5 +14,64 @@ public class FilmeApi : ControllerBase
     {
         _filmeService = filmeService;
     }
-}
 
+    [HttpPost]
+    public IActionResult Create(Filme filme)
+    {
+        if(_filmeService.SalvarFilme(filme))
+        {
+            return Ok();
+        }
+        else
+        {
+            return BadRequest("Falha ao salvar");
+        }
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> Get()
+    {
+        return _filmeService.ListarFilmes();
+    }
+
+    [HttpPut]
+    public IActionResult Update(filme Filme)
+    {
+        if (_filmeService.AlterarFilme(filme))
+        {
+            return Ok();
+        }
+        else
+        {
+            return BadRequest("Falha ao atualizar o filme");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        var filme = _filmeService.ObterFilme(id);
+
+        if (filme == null)
+        {
+            return NotFound("falha ao encontrar o filme");
+        }
+        else
+        {
+            return Ok(filme);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        if (_filmeService.DeletarFilme(id))
+        {
+            return Ok("Livro deletado");
+        }
+        else
+        {
+            return BadRequest("");
+        }
+    }
+}
